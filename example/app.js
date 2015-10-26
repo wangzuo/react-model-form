820df432be13ad20524b3d9c09fea844dda3afda
wang zuo <wzuoadjusted@gmail.com>
2015-10-26 21:25:47 +0800

    add input-switch.less

--- example/app.js
+++ example/app.js
@@ -1,9 +1,11 @@
 require('../lib/form.less');
+require('../lib/input-switch.less');
 require('./app.less');
 
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Form = require('../lib/form');
+var packageInfo = require('../package.json');
 
 var App = React.createClass({
   displayName: 'App',
@@ -12,7 +14,9 @@ var App = React.createClass({
     return {
       model: {
         attrs: [
-          {name: 'title', type: 'string', label: 'Title'}
+          {name: 'title', type: 'string', label: 'Title'},
+          {name: 'content', type: 'text', label: 'content'},
+          {name: 'published', type: 'boolean', label: 'Published'}
         ],
         values: {
           title: 'Sit Tristique Mollis'
@@ -24,13 +28,14 @@ var App = React.createClass({
   render() {
     return (
       <div className="app">
-        <h1>react-model-form</h1>
+        <h1>{packageInfo.name}</h1>
+        <h2>{packageInfo.description}</h2>
         <Form
           model={this.state.model}
           onChange={this.handleChange}
         />
         <pre>
-          {JSON.stringify(this.state.model, null, '  ')} 
+          {JSON.stringify(this.state.model, null, '  ')}
         </pre>
       </div>
     );
