3e6cf43fe30852ea0c66f16eb3321772040394cd
Wang Zuo <wzuoadjusted@gmail.com>
2015-10-28 15:55:25 +0800

    fix onchange

--- example/app.js
+++ example/app.js
@@ -1,13 +1,20 @@
 require('react-input-color/lib/input-color.less');
 require('react-input-switch/lib/switch.less');
 require('react-input-slider/lib/input-slider.less');
+require('../lib/select.less');
 require('../lib/form.less');
 require('./app.less');
 
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Form = require('../lib/form');
-var packageInfo = require('../package.json');
+var _package = require('../package.json');
+
+var categories = [
+  {name: 'news', id: '123'},
+  {name: 'tech', id: '124'},
+  {name: 'sports', id: '125'}
+];
 
 var App = React.createClass({
   displayName: 'App',
@@ -15,7 +22,11 @@ var App = React.createClass({
   getInitialState() {
     return {
       model: {
+        values: {
+          title: 'Sit Tristique Mollis'
+        },
         attrs: [
+          {name: 'categories', type: 'select', label: 'Categories', options: categories, multiple: true},
           {name: 'published', type: 'boolean', label: 'Published'},
           {name: 'password', type: 'password', label: 'Password'},
           {name: 'cover', type: 'file', label: 'Cover image'},
@@ -23,10 +34,7 @@ var App = React.createClass({
           {name: 'content', type: 'text', label: 'content'},
           {name: 'backgroundColor', type: 'color', label: 'Background Color'},
           {name: 'age', type: 'range', label: 'Age', min: 12, max: 48}
-        ],
-        values: {
-          title: 'Sit Tristique Mollis'
-        }
+        ]
       }
     };
   },
@@ -34,17 +42,21 @@ var App = React.createClass({
   render() {
     return (
       <div className="app">
-        <h1>{packageInfo.name}</h1>
-        <h2>{packageInfo.description}</h2>
-        <Form
-          method="post"
-          model={this.state.model}
-          onChange={this.handleChange}>
-          <input type="submit" value="Create post"/>
-        </Form>
-        <pre>
-          {JSON.stringify(this.state.model, null, '  ')}
-        </pre>
+        <div className="col">
+          <h1>{_package.name}</h1>
+          <h2>{_package.description}</h2>
+          <Form
+            method="post"
+            model={this.state.model}
+            onChange={this.handleChange}>
+            <input type="submit" value="Create post"/>
+          </Form>
+        </div>
+        <div className="col">
+          <pre>
+            {JSON.stringify(this.state.model, null, '  ')}
+          </pre>
+        </div>
       </div>
     );
   },
