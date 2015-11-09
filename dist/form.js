48415d667d5b9d17110b9c9186e115498dd4c898
Wang Zuo <wzuoadjusted@gmail.com>
2015-11-09 18:11:51 +0800

    update builds

--- dist/form.js
+++ dist/form.js
@@ -2,40 +2,58 @@
 
 var blacklist = require('blacklist');
 var React = require('react');
-var Element = require('./element');
 
-module.exports = React.createClass({
-  displayName: 'ModelForm',
+module.exports = function (elements) {
 
-  getDefaultProps: function getDefaultProps() {
-    return {
-      model: {
-        attrs: [],
-        values: {}
-      }
+  var Element = function Element(props) {
+    var attr = props.attr;
+    var value = props.value;
+    var onChange = props.onChange;
+
+    var type = attr.type;
+    var name = attr.name;
+    var label = attr.label;
+    var handleChange = function handleChange(value) {
+      onChange(name, value);
     };
-  },
-  renderAttr: function renderAttr(attr, i) {
-    var model = this.props.model;
-    var values = model.values || {};
 
-    return React.createElement(Element, {
-      key: i,
-      attr: attr,
-      value: values[attr.name],
-      onChange: this.props.onChange
-    });
-  },
-  render: function render() {
-    var model = this.props.model;
-    var attrs = model.attrs;
-    var props = blacklist(this.props, 'onChange', 'children');
+    if (!elements[type]) throw new Error('type ' + type + ' element not defined');
+    return elements[type](attr, value, handleChange);
+  };
 
-    return React.createElement(
-      'form',
-      props,
-      attrs.map(this.renderAttr),
-      this.props.children
-    );
-  }
-});
+  return React.createClass({
+    displayName: 'ModelForm',
+
+    getDefaultProps: function getDefaultProps() {
+      return {
+        model: {
+          attrs: [],
+          values: {}
+        }
+      };
+    },
+    renderAttr: function renderAttr(attr, i) {
+      var model = this.props.model;
+      var values = model.values || {};
+
+      return React.createElement(Element, {
+        key: i,
+        attr: attr,
+        value: values[attr.name],
+        onChange: this.props.onChange
+      });
+    },
+    render: function render() {
+      var model = this.props.model;
+      var attrs = model.attrs;
+      var props = blacklist(this.props, 'onChange', 'children');
+
+      return React.createElement(
+        'form',
+        props,
+        attrs.map(this.renderAttr),
+        this.props.children
+      );
+    }
+  });
+};
